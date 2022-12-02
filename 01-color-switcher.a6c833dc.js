2626b75f2e64e0dc4fe65cfd6e38bf36508c94a0
Sasha92Shevchuk <ol.shevchuk92@gmail.com>
2022-12-02 18:23:20 +0000

    Deploying to gh-pages from @ Sasha92Shevchuk/goit-js-hw-09@7014470dd6fdf4542b80c029fada538e5e772ea9 🚀

---
+++ 01-color-switcher.a6c833dc.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]");console.log("btnStart",t);
+//# sourceMappingURL=01-color-switcher.a6c833dc.js.map
---
+++ 01-color-switcher.acaff17f.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]");console.log("btnStart",t)}();
+//# sourceMappingURL=01-color-switcher.acaff17f.js.map
