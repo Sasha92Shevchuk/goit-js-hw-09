f82451aa64c630265b578081e778135a5353a14d
Sasha92Shevchuk <108683139+Sasha92Shevchuk@users.noreply.github.com>
2022-12-02 21:49:32 +0200

    done first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,2 +1,30 @@
-const btnStart = document.querySelector('[data-start]');
-console.log('btnStart', btnStart);
+const startRef = document.querySelector('[data-start]');
+const stopRef = document.querySelector('[data-stop]');
+const bodyRef = document.body;
+
+startRef.addEventListener('click', onStartChangeColor);
+stopRef.addEventListener('click', onStopChangeColor);
+
+let checkStartRef = true;
+let timerId = null;
+
+function onStartChangeColor() {
+  if (checkStartRef === false) {
+    return;
+  }
+  checkStartRef = false;
+
+  timerId = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  console.log('click on start');
+}
+
+function onStopChangeColor() {
+  clearInterval(timerId);
+  checkStartRef = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
